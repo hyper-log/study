eeb8956451197737eacb332f718de79dd2de576a
hyper-log <revoluer9@gmail.com>
2023-03-30 01:50:17 +0900

    최대값, 최소값 구하기

---
+++ java_study/src/java_study/maxmin.java
@@ -0,0 +1,39 @@
+package java_study;
+
+public class maxmin {
+	
+	//최대값, 최소값 구하기
+	
+	public static void main(String[] args) {
+		
+		int num[] = {5, 10, 15, 3, 4};
+		int max=0;
+		int min=0;
+		
+		/* 최대값 */
+		for(int i=0; i<num.length; i++) {
+			if(num[i]>num[0]) {
+				int temp = num[0];
+				num[0] = num[i];
+				num[i] = temp;
+				max=num[0];
+			}
+			
+		}	
+		
+		/* 최소값 */
+		for(int i=0; i<num.length; i++) {
+			if(num[i]<num[0]) {
+				int temp = num[0]; 
+				num[0] = num[i];
+				num[i] = temp;
+				min=num[0];
+			}
+		}
+			
+		
+		System.out.println(max);
+		System.out.println(min);
+		
+	}
+}
