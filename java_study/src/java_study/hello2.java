15409bc98023b76d732d079d5d768ff69406d20c
hyper-log <revoluer9@gmail.com>
2023-03-30 00:02:03 +0900

    재반영 도전

--- java_study/src/java_study/hello2.java
+++ java_study/src/java_study/hello2.java
@@ -2,6 +2,6 @@ package java_study;
 
 public class hello2 {
 	public static void main(String[] args) {
-		System.out.println("잔디가 왜 안 심어질까, 재반영 도전");
+		System.out.println("잔디가 왜 안 심어질까, 재반영 도전!");
 	}
 }
